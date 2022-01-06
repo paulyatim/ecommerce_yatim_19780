8a591d554c1332e9c389dc4270ad764aa83bb561
paulyatim <jeanpaulyatimg@gmail.com>
2022-01-06 10:16:22 -0300

    detalles minimos

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,6 +2,7 @@ import {useContext} from 'react'
 import { CartContext } from '../context/CartContext'
 import {CartItem} from './CartItem'
 import EmptyCart from './EmptyCart'
+import {getFirestore, collection, addDoc} from 'firebase/firestore'
 
 function Cart() {
 
@@ -9,6 +10,22 @@ function Cart() {
     
     if (cartList.length === 0) {
         return <EmptyCart/>
+    }
+
+    function checkOut() {
+        let itemsCheckOut = []
+        cartList.map(prod => itemsCheckOut.push({id: prod.id, name: prod.name, price: prod.price, quantity: prod.quantity}))
+
+        let order = {buyer: {name: "Paul", phone: "12345678", mail: "paul@mail.com"}, 
+                     items: itemsCheckOut, 
+                     total: totalPrice()}
+        
+        const db = getFirestore()
+        const orderCollection  = collection(db, 'orders')
+        addDoc(orderCollection, order)
+        .then(resp => console.log(resp))
+        .catch(error => console.log(error))
+        // console.log(order)
     }
 
     return (
@@ -19,7 +36,7 @@ function Cart() {
             </div>
             <h3 className='totalPriceCart'>Total: $ {totalPrice()}</h3>
             <button onClick={clearCart} type="button" className="btn btn-outline-primary btnCart">Clear</button>
-            <button type="button" className="btn btn-outline-primary btnCart">Check-out</button>
+            <button onClick={checkOut} type="button" className="btn btn-outline-primary btnCart">Check-out</button>
         </div>
     )
 }
